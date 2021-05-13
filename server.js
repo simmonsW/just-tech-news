6eede6345a6bded3f9beb1964f18151ea65ad74f
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-12 23:36:07 -0700

    added stylesheet

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -5,6 +6,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

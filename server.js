e39089d810ecd7be8b9915c163c8852dbd50d433
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-14 16:15:58 -0700

    restructed server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const session = require('express-session');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -22,13 +19,20 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = require('./controllers');
 
 // turn on routes
 app.use(routes);

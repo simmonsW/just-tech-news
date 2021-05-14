8254acfcf133a1cfb8b6efe3155c3cf8cde04e4e
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-14 16:10:38 -0700

    implemented helper functions

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 

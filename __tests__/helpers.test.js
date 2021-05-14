64717b74b028db85a1afc953771e78445b459eb6
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-14 15:55:13 -0700

    created format_plural and test

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,18 @@
-const {format_date} = require('../utils/helpers');
+const { format_date, format_plural } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
   const date = new Date('2020-03-20 16:12:03');
 
-  expect(format_date(date)).toBe('3/20/2020')
+  expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() returns a pluralized word', () => {
+  const word = 'Tiger';
+  let amount = 1;
+
+  expect(format_plural(word, amount)).toMatch('Tiger');
+
+  amount = 2;
+
+  expect(format_plural(word, amount)).toMatch('Tigers');
 });
